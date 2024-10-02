c27652d0a7eecfc6d6e5028eff92ac15d0dded1f
matiszac <zcornilliac@gmail.com>
2024-10-01 20:40:59 -0400

    add BLACK to constants, add pygame boiler plate

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    #print("Starting asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    pygame.event.pump()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(BLACK)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
