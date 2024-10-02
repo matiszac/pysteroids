73feaaece97cae7c163c02b2420542c9fff174f5
matiszac <zcornilliac@gmail.com>
2024-10-01 21:15:40 -0400

    create player class and render it in main game loop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 from constants import *
 
@@ -7,15 +8,27 @@ def main():
     #print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # game loop
-    pygame.event.pump()
+    pygame.event.pump() # no screen opens on mac without this for some reason
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill(BLACK)
+
+        player.draw(screen)
+        # draw calls above this line
         pygame.display.flip()
+        # tick returns time passed since last frame, / 1000 to convert to ms
+        dt = clock.tick(60) / 1000 # tick(60) pause for 1/60th of a second
+
 
 
 if __name__ == "__main__":
