ddd6c7e7d095dd8db9d311df3fd5caa294a4f8a8
matiszac <zcornilliac@gmail.com>
2024-10-01 20:04:35 -0400

    add constants.py update gitignore __pycache__/

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 
+from constants import *
+
 def main():
     print("Starting asteroids!")
-
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
